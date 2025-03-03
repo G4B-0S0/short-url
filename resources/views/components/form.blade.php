0d9d2005e85cbf7749787b19ceecf498f7259c77
Gabo <gyturrino@gmail.com>
2025-03-02 19:04:59 -0500

    Diseño e interfaz basico

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,4 @@
+<form action="" method="post" class="grid grid-row-2 gap-4">
+    <input type="text" name="url" placeholder="Enter URL" class="border border-gray-300 rounded p-2">
+    <button type="submit" class="p-2 border border-gray-300 rounded">Shorten</button>
+</form>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @vite('resources/css/app.css')
+    <title>URL Shortener</title>
 </head>
 <body>
     
+    <main class="container mx-auto p-4 content-center">
+        <div class="form grid grid-cols-3 gap-4 mb-10">
+            <div></div>
+            <div>
+                <h1 class="text-4xl text-center mb-4">URL Shortener</h1>
+                <x-form />
+            </div>
+            <div></div>
+        </div>
+
+    <div class="url-results">
+        <h2 class="text-xl mb-4">URL's acortadas</h2>
+    
+    {{-- <div>
+        <h2>Shortened URLs</h2>
+        <ul>
+            @foreach ($urls as $url)
+                <li>
+                    <a href="{{ $url->shortened }}">{{ $url->shortened }}</a>
+                    <span>({{ $url->original }})</span>
+                </li>
+            @endforeach
+        </ul>
+    </div> --}}
+
+    <x-tabla/>
+    </div>
+    </main>
+
 </body>
 </html>
