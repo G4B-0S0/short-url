71cad75730385c34b5f0382366c733acaf0a39f5
Gabo <gyturrino@gmail.com>
2025-03-05 17:30:35 -0500

    Estilización y mode theme

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,4 +1,4 @@
 <form action="" method="post" class="grid grid-row-2 gap-4">
-    <input type="text" name="url" placeholder="Enter URL" class="border border-gray-300 rounded p-2">
-    <button type="submit" class="p-2 border border-gray-300 rounded">Shorten</button>
+    <input type="text" name="url" placeholder="Enter URL" class="border border-gray-300 rounded p-2 dark:placeholder-gray-300">
+    <button type="submit" class="p-2 border border-gray-300 rounded bg-gray-800 text-gray-200">Shorten</button>
 </form>
---
+++ resources/views/components/result.blade.php
@@ -0,0 +1,17 @@
+<div class="url-results">
+    <h2 class="text-xl mb-4 text-gray-800  dark:text-gray-300">URL's acortadas</h2>
+
+            {{-- <div>
+        <h2>Shortened URLs</h2>
+        <ul>
+            @foreach ($urls as $url)
+                <li>
+                    <a href="{{ $url->shortened }}">{{ $url->shortened }}</a>
+                    <span>({{ $url->original }})</span>
+                </li>
+            @endforeach
+        </ul>
+    </div> --}}
+
+    <x-tabla />
+</div>
